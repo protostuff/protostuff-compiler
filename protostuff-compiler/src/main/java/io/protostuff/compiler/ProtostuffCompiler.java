cd9aa3c6523f8dfb9793092f23c6ed281c7e417e
Kostiantyn Shchepanovskyi <schepanovsky@gmail.com>
2015-07-16 22:48:37 +0300

    Change project structure in order to simplify result artifact names

---
+++ protostuff-compiler/src/main/java/io/protostuff/compiler/ProtostuffCompiler.java
@@ -0,0 +1,11 @@
+package io.protostuff.compiler;
+
+/**
+ * @author Kostiantyn Shchepanovskyi
+ */
+public class ProtostuffCompiler {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
