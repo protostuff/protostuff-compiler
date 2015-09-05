d80c751c65fc46e4972806240d770dfb249c0d72
Kostiantyn Shchepanovskyi <schepanovsky@gmail.com>
2015-09-05 12:36:59 +0300

    Maven plugin - use slf4j logger API

--- protostuff-compiler-maven-plugin/src/main/java/io/protostuff/compiler/maven/ProtostuffCompilerMojo.java
+++ protostuff-compiler-maven-plugin/src/main/java/io/protostuff/compiler/maven/ProtostuffCompilerMojo.java
@@ -4,19 +4,23 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.StaticLoggerBinder;
 
 import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_SOURCES;
 
 /**
  * @author Kostiantyn Shchepanovskyi
  */
-@Mojo(name = "compile", defaultPhase = GENERATE_SOURCES)
+@Mojo(name = "generate", defaultPhase = GENERATE_SOURCES)
 public class ProtostuffCompilerMojo extends AbstractMojo {
 
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProtostuffCompilerMojo.class);
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("protostuff compiler");
+        StaticLoggerBinder.getSingleton().setMavenLog(this.getLog());
+        LOGGER.info("Running protostuff compiler");
     }
 
 }
