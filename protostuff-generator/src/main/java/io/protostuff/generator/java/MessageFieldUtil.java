596dded89024a08fb08faf37732c2fe706714a70
Kostiantyn Shchepanovskyi <schepanovsky@gmail.com>
2015-09-30 00:11:34 +0300

    #7: Java code generator
    Generate builder to construct message, set non-repeated fields only.

--- protostuff-generator/src/main/java/io/protostuff/generator/java/MessageFieldUtil.java
+++ protostuff-generator/src/main/java/io/protostuff/generator/java/MessageFieldUtil.java
@@ -2,6 +2,7 @@ package io.protostuff.generator.java;
 
 import io.protostuff.compiler.model.Field;
 import io.protostuff.compiler.model.FieldType;
+import io.protostuff.compiler.model.Message;
 import io.protostuff.compiler.model.ScalarFieldType;
 import io.protostuff.compiler.model.UserType;
 import io.protostuff.generator.Formatter;
@@ -10,6 +11,9 @@ import io.protostuff.generator.Formatter;
  * @author Kostiantyn Shchepanovskyi
  */
 public class MessageFieldUtil {
+
+    public static final String GETTER_PREFIX = "get";
+    public static final String CHECK_SET_PREFIX = "has";
 
     public static String getFieldType(Field field) {
         FieldType type = field.getType();
@@ -27,5 +31,21 @@ public class MessageFieldUtil {
     public static String getFieldName(Field field) {
         String name = field.getName();
         return Formatter.toCamelCase(name);
+    }
+
+    public static String getFieldGetterName(Field field) {
+        return GETTER_PREFIX + Formatter.toPascalCase(field.getName());
+    }
+
+    public static boolean isGenerateHasMethod(Field field) {
+        return field.getType() instanceof Message;
+    }
+
+    public static String getHasMethodName(Field field) {
+        return CHECK_SET_PREFIX + Formatter.toPascalCase(field.getName());
+    }
+
+    public static String getBuilderSetterName(Field field) {
+        return Formatter.toPascalCase(field.getName());
     }
 }
